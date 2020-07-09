ffda2710abb7bae4e556256953fd85d804791233
Maria Fatima Rubi Gzz Gzz <fatima2000g3@gmail.com>
2020-07-09 10:24:00 -0500

    prog2 finalizado

---
+++ prog2.py
@@ -0,0 +1,12 @@
+
+
+
+print ("Programa que muestra la suma de 2 numeros enteros") 
+num1 = int(input("Ingrese el primer numero a sumar:")) 
+num2 = int(input("Ingrese el segundo numero a sumar:")) 
+suma = 0
+i = num1
+while (i <= num2) :
+    suma = suma + i
+    i = i + 1
+print("El resultado de su suma es:",suma) 
